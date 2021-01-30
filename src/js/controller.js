f4f93fd1061404057f73fced3e301b02c9cf1a08
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-01-29 20:19:00 -0800

    created config and helper js file and refactored code using MVC architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,24 +1,13 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
-
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-
 
 const controlRecipes = async function () {
   try {
@@ -36,5 +25,7 @@ const controlRecipes = async function () {
   };
 };
 
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
-// window.addEventListener('hashchange', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -29,6 +29,10 @@ class ReviewView {
         this.#parentElement.insertAdjacentHTML('afterbegin', markup);
     }
 
+    addHandlerRender(handler) {
+        ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+    }
+
     #generateMarkup() {
         return `
         <figure class="recipe__fig">
