e903d5f308755917930aa705ed97b9a32af57583
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-02-01 16:10:29 -0800

    created pagination button and rendered pages on click event based on the data attribute to connect DOM and jS

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime/runtime';
@@ -46,14 +47,26 @@ const controlSearchResults = async function () {
     // resultsView.render(model.state.search.results)
     resultsView.render(model.getSearchResultsPage());
 
+    // 4) Render Initial Pagination
+    paginationView.render(model.state.search)
+
 
   } catch (err) {
     console.log(err)
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 1) Render search result
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render Initial Pagination
+  paginationView.render(model.state.search)
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
